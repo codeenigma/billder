ecf56c8e6975fcf6be9331ccba6cb0d34b51ac03
Greg Harvey <greg.harvey@gmail.com>
2017-10-27 11:54:41 +0200

    Improving documentation.

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const invoices = require('./lib/billder-make-invoice');
 const auth = require('./lib/billder-freeagent-auth');
 var syslog = require('modern-syslog');
 
+/*
+ * Because we expect to run this monthly, we'll assume our
+ * access token has expired and get a new one.
+ */
 syslog.notice('Billder: refreshing OAuth access token');
 console.log('Billder: refreshing OAuth access token');
 
 auth.refresh_oauth_token();
 
+/*
+ * Now we have a fresh access token we can make invoices!
+ */
 syslog.notice('Billder: starting invoicing run');
 console.log('Billder: starting invoicing run');
 
