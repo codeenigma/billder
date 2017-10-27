6cc21c7af94251e487d14faf0791982b24d203c2
Greg Harvey <greg.harvey@gmail.com>
2017-10-27 11:37:27 +0200

    Logging to syslog and tidying up output.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+
 /*
- * Start here!
+ * Trigger the invoicing process with this script.
  */
 
 const invoices = require('./lib/billder-make-invoice');
+const auth = require('./lib/billder-freeagent-auth');
+var syslog = require('modern-syslog');
+
+syslog.notice('Billder: refreshing OAuth access token');
+console.log('Billder: refreshing OAuth access token');
+
+auth.refresh_oauth_token();
+
+syslog.notice('Billder: starting invoicing run');
+console.log('Billder: starting invoicing run');
 
 invoices.make_invoices();
 
