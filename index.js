b273e21ab9e14fea88aca67c6d3dc3194d73c070
Greg Harvey <greg.harvey@gmail.com>
2017-11-23 21:27:12 +0100

    Adding 'month' support so you can pass a month to generate invoices for.

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ var provider_path = './lib/provider/' + config.provider.name + '/';
 const invoices = require('./lib/billder-make-invoice');
 const auth = require(provider_path + 'billder-auth');
 var syslog = require('modern-syslog');
+var now = new Date();
+var pad = require('pad');
+
+// Get the month to process from the CLI
+var args = process.argv.slice(2);
+if (args[0]) {
+  var month = args[0];
+  syslog.notice('Billder: we received a month to process: %s', month);
+  console.log('Billder: we received a month to process: %s', month);
+}
+else {
+  // getMonth() gives us month number - 1 so January is 0
+  if (now.getMonth() == 0) {
+    var year = now.getFullYear() - 1;
+    var lastMonth = 12;
+  }
+  else {
+    var year = now.getFullYear();
+    var lastMonth = pad(2, now.getMonth(), '0');
+  }
+  // Defaults to last month
+  var month = year + '-' + lastMonth;
+
+  syslog.notice('Billder: we did not receive a month to process, defaulting to last month: %s', month);
+  console.log('Billder: we did not receive a month to process, defaulting to last month: %s', month);
+}
 
 /*
  * Because we expect to run this monthly, we'll assume our
@@ -24,5 +50,5 @@ auth.authenticate();
 syslog.notice('Billder: starting invoicing run');
 console.log('Billder: starting invoicing run');
 
-invoices.make_invoices();
+invoices.make_invoices(month);
 
